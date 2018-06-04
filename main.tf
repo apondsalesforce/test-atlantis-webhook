0ff92c3ae357c7e966ba7d23929d1359e24a3a84
winston-tong-SFDC <wtong@salesforce.com>
2018-06-04 13:54:16 -0700

    updating vpc id

--- main.tf
+++ main.tf
@@ -4,7 +4,8 @@ resource "null_resource" "example" {
 resource "aws_security_group" "winstontest" {
   name        = "winstontest"
   description = "winstontest"
-  
+  vpc_id      = "vpc-6329ac05"
+    
   tags {
     Name = "winstontest"
   }
