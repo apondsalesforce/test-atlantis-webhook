9c684f04dde4284c98a5a70e79108f1848624955
LaDean <DeanDean12@users.noreply.github.com>
2018-06-04 14:43:35 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,9 @@ resource "null_resource" "examples" {
 resource "null_resource" "example2" {
 }
 
+resource "null_resource" "example3" {
+}
+
 resource "aws_security_group" "winstontest" {
   name        = "winstontest"
   description = "winstontest"
